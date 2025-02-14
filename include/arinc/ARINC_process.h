8318e93a47da4796ab2d7e7faad60d90bd3bff66
salma-sah <etudessalma@gmail.com>
2025-02-14 15:48:16 -0500

    add arinc process header file

---
+++ include/arinc/ARINC_process.h
@@ -0,0 +1,144 @@
+#include <ARINC_apex_types.h>
+
+/*----------------------------------------------------------------*/
+/*                                                                */
+/* process constant and type definitions and management services  */
+/*                                                                */
+/*----------------------------------------------------------------*/
+
+#ifndef apex_process
+#define apex_process
+
+#define max_number_of_processes system_limit_number_of_processes
+
+#define min_priority_value 1
+
+#define max_priority_value 239
+
+#define max_lock_level 16
+
+typedef name_type process_name_type;
+
+typedef apex_integer process_id_type;
+#define null_process_id 0
+
+// TODO review main process id value
+#define main_process_id 1
+
+typedef apex_integer process_index_type;
+
+typedef apex_integer lock_level_type;
+
+typedef apex_unsigned stack_size_type;
+
+typedef apex_integer waiting_range_type;
+
+typedef apex_integer priority_type;
+
+typedef enum
+{
+       dormant = 0,
+       ready = 1,
+       running = 2,
+       waiting = 3,
+       faulted = 4
+} process_state_type;
+
+typedef enum
+{
+       soft = 0,
+       hard = 1
+} deadline_type;
+
+typedef struct
+{
+       system_time_type period;
+       system_time_type time_capacity;
+       system_address_type entry_point;
+       stack_size_type stack_size;
+       priority_type base_priority;
+       deadline_type deadline;
+       process_name_type name;
+} process_attribute_type;
+
+typedef struct
+{
+       system_time_type deadline_time;
+       priority_type current_priority;
+       process_state_type process_state;
+       process_attribute_type attributes;
+} process_status_type;
+
+extern void create_process(
+    /*in */ process_attribute_type *attributes,
+    /*out*/ process_id_type *process_id,
+    /*out*/ return_code_type *return_code);
+
+extern void set_priority(
+    /*in */ process_id_type process_id,
+    /*in */ priority_type priority,
+    /*out*/ return_code_type *return_code);
+
+extern void suspend_self(
+    /*in */ system_time_type time_out,
+    /*out*/ return_code_type *return_code);
+
+extern void suspend(
+    /*in */ process_id_type process_id,
+    /*out*/ return_code_type *return_code);
+
+extern void resume(
+    /*in */ process_id_type process_id,
+    /*out*/ return_code_type *return_code);
+
+extern void stop_self(void);
+
+extern void stop(
+    /*in */ process_id_type process_id,
+    /*out*/ return_code_type *return_code);
+
+extern void start(
+    /*in */ process_id_type process_id,
+    /*out*/ return_code_type *return_code);
+
+extern void delayed_start(
+    /*in */ process_id_type process_id,
+    /*in */ system_time_type delay_time,
+    /*out*/ return_code_type *return_code);
+
+extern void lock_preemption(
+    /*out*/ lock_level_type *lock_level,
+    /*out*/ return_code_type *return_code);
+
+extern void unlock_preemption(
+    /*out*/ lock_level_type *lock_level,
+    /*out*/ return_code_type *return_code);
+
+extern void get_my_id(
+    /*out*/ process_id_type *process_id,
+    /*out*/ return_code_type *return_code);
+
+extern void get_process_id(
+    /*in */ process_name_type process_name,
+    /*out*/ process_id_type *process_id,
+    /*out*/ return_code_type *return_code);
+
+extern void get_process_status(
+    /*in */ process_id_type process_id,
+    /*out*/ process_status_type *process_status,
+    /*out*/ return_code_type *return_code);
+
+extern void initialize_process_core_affinity(
+    /*in */ process_id_type process_id,
+    /*in */ processor_core_id_type processor_core_id,
+    /*out*/ return_code_type *return_code);
+
+extern void get_my_processor_core_id(
+    /*out*/ processor_core_id_type *processor_core_id,
+    /*out*/ return_code_type *return_code);
+
+extern void get_my_index(
+    /*out*/ process_index_type *process_index,
+    /*out*/ return_code_type *return_code);
+
+#endif
