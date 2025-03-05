25e256d4293f914cf5778c0dbdf294860447541d
salma-sah <etudessalma@gmail.com>
2025-03-04 21:08:51 -0500

    add partition creation test app

--- app/arinc.c
+++ app/arinc.c
@@ -1,24 +1,27 @@
 #include <ucx.h>
-#include <limits.h>
+void test_mos_creation() {
+	uint16_t mos_id;
+	return_code_type return_code;
+	partition_id_type first_partition_id;
+
+    create_mos(&mos_id, &return_code);
+    printf("MOS CREATED");
+    
+	system_address_type fisrt_app = (void*)test_create_process;
+	add_new_partition(mos_id,fisrt_app,&first_partition_id, &return_code);
+    printf("TEST CREATE PROCESS PARTITION CREATED");
+}
 
 void test_create_process() {
     process_attribute_type attributes;
     process_id_type process_id;
     return_code_type return_code;
-
-    // Valid parameters
     attributes.stack_size = 1024;
     attributes.base_priority = 10;
     attributes.period = 1000;
     attributes.time_capacity = 500;
     create_process(&attributes, &process_id, &return_code);
     printf("Test 1 : return_code = %d\n", return_code);
-
-    // time_capacity greater than period
-    attributes.base_priority = 10;
-    attributes.time_capacity = 2000;
-    create_process(&attributes, &process_id, &return_code);
-    printf("Test 4 : return_code = %d\n", return_code);
 }
 
 int32_t app_main(void)
