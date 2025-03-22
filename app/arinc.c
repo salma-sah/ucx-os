4e23473780bccb725e80913cb86a4817dcbcf7c1
salma-sah <etudessalma@gmail.com>
2025-03-22 14:23:14 -0400

    update arinc app

--- app/arinc.c
+++ app/arinc.c
@@ -12,21 +12,41 @@ void test_create_process() {
     printf("Test 1 : return_code = %d\n", return_code);
 }
 
-void test_mos_creation() {
+void test_task(void)
+{
+    printf("Executing test task in partition\n");
+}
+
+void test_mos_and_partitions_creation() {
 	uint16_t mos_id;
 	return_code_type return_code;
 	partition_id_type first_partition_id;
 
     create_mos(&mos_id, &return_code);
-    printf("MOS CREATED\n");
+    if (return_code != NO_ERROR)
+    {
+        printf("[Error] Failed to create MOS\n");
+        return;
+    }
+    printf("[Success] MOS created with ID: %d\n", mos_id);
     
-	system_address_type fisrt_app = (void*)test_create_process;
-	add_new_partition(mos_id,fisrt_app,&first_partition_id, &return_code);
+	system_address_type fisrt_app = (void*)test_task;
+	add_new_partition(fisrt_app,&first_partition_id, &return_code);
     printf("TEST CREATE PROCESS PARTITION CREATED\n");
+
+    if (return_code != NO_ERROR)
+    {
+        printf("[Error] Failed to add partition\n");
+        return;
+    }
+    printf("[Success] Partition created with ID: %d\n", first_partition_id);
+
+    printf("[Info] Starting partition scheduling...\n");
+    schedule_partitions();
 }
 
 int32_t app_main(void)
 {
-	test_mos_creation();
+	test_mos_and_partitions_creation();
 	return 0;
 }
